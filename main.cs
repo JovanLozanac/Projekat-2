e4bd99c5197e04717abaac8b3672452c95696831
Jovan Lozanac <>
2021-04-12 13:42:57 +0000

    Dodata metoda za pravljenje prvog naloga i pomoćne metode: za (de)šifrovanje i za proveru da li su samo mala i velika slova u stringu

--- main.cs
+++ main.cs
@@ -23,37 +23,40 @@ class MainClass
 		Console.WriteLine("===================================================================================================");
 		//Provera i logovanje:
 		string nazivFajla = "korisnici.txt";
+		string unos;
 		if(File.Exists(nazivFajla)){
 			Console.WriteLine("Dobrodošli! Ulogujte se (1), napravite novi nalog (2) ili izađite iz programa (3)");
 			do{
-				if(Console.ReadLine() == "1"){
+				unos = Console.ReadLine();
+				if(unos == "1"){
 					UlogujSe();
 				}
-				else if (Console.ReadLine() == "2"){
+				else if (unos == "2"){
 					NapraviNoviNalog();
 				}
-				else if (Console.ReadLine() == "3"){
+				else if (unos == "3"){
 					Console.WriteLine("Hvala Vam što ste koristili naš program. Doviđenja!");
 				}
 				else {
 					Console.WriteLine("Molimo Vas, ponovite unos.");
 				}
-			}while(Console.ReadLine() != "1" && Console.ReadLine() != "2" && Console.ReadLine() != "3");
+			}while(unos != "1" && unos != "2" && unos != "3");
 			
 		}
 		else{
 			Console.WriteLine("Čestitamo! Vi ste naš prvi korisnik. Napravite nalog (1) ili izađite iz programa (2).");
 			do{
-				if(Console.ReadLine() == "1"){
+				unos = Console.ReadLine();
+				if(unos == "1"){
 					NapraviPrviNalog();
 				}
-				else if (Console.ReadLine() == "2"){
+				else if (unos == "2"){
 					Console.WriteLine("Hvala Vam što ste koristili naš program. Doviđenja!");
 				}
 				else {
 					Console.WriteLine("Molimo Vas, ponovite unos.");
 				}
-			}while(Console.ReadLine() != "1" && Console.ReadLine() != "2");
+			}while(unos != "1" && unos != "2");
 			
 		}
 	}
@@ -64,7 +67,66 @@ class MainClass
 		//napraviti metodu
 	}
 	public static void NapraviPrviNalog(){
-		//napraviti metodu
+		bool Uslov;
+		do{
+			Uslov = true;
+			Console.Clear();
+			Console.WriteLine("Upišite korisničko ime. Ono se sastoji samo iz slova engleske abecede.");
+			string username = Console.ReadLine();
+			while(username == "" || NisuSamoSlova(username)){
+				Console.WriteLine("Probajte ponovo.");
+				username = Console.ReadLine();
+			}
+			Console.WriteLine("Upišite lozinku. Ona se sastoji samo iz slova engleske abecede.");
+			string password = Console.ReadLine();
+			while(password == "" || NisuSamoSlova(password)){
+				Console.WriteLine("Probajte ponovo.");
+				password = Console.ReadLine();
+			}
+			string unos;
+			do{
+				Console.Clear();
+				Console.WriteLine("Korisničko ime: " + username);
+				Console.WriteLine("Lozinka: " + password);
+				Console.WriteLine("Da li potvrđujete ove podatke? (1 - da, 2 - ne)");
+				unos = Console.ReadLine();
+				if(unos == "1"){
+					StreamWriter podaci = new StreamWriter ("korisnici.txt");
+					podaci.WriteLine("username:{0}|password:{1}", DeSifruj(username, 1), DeSifruj(password, 1));
+					podaci.Close();
+					PocetniEkranILogovanje();
+				}
+				else if(unos == "2") {
+					Uslov = false;
+				}
+			}while(unos != "1" && unos != "2");
+		}while(!Uslov);
+	}
+	public static string DeSifruj (string rec, int smer){
+		//smer = 1: sifruj rec
+		//smer = 2: DEsifruj rec
+		string slovaVelika = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		string slovaMala = slovaVelika.ToLower();
+		
+		int pomak = 5;
+		char[] kodirana = new char[rec.Length];
+		if (smer == 2) pomak *= -1;
+		for (int i = 0; i < rec.Length; i++){
+			char trenutnoslovo = rec[i];
+			char maloslovo = char.ToLower(trenutnoslovo);
+			if(trenutnoslovo.CompareTo(maloslovo) == 0) kodirana[i] = slovaMala[(26 + (slovaMala.IndexOf(rec[i]) + pomak)) % 26];
+			else kodirana[i] = slovaVelika[(26 + (slovaVelika.IndexOf(rec[i]) + pomak)) % 26];
+		}
+		string NovaRec = new string(kodirana);
+		return NovaRec;
+	}
+	public static bool NisuSamoSlova(string rec){
+		//false: samo su slova
+		//true: nisu samo slova
+		for(int i = 0; i<rec.Length; i++){
+			if(!char.IsLetter(rec[i])) return true;
+		}
+		return false;
 	}
   public static void Main () 
   {
