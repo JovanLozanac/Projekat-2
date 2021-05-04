d5f0d076556c3b426e401a4122f1a1400f90b1fa
Jovan Lozanac <>
2021-05-04 15:03:12 +0000

    S --> Š na jednom mestu

--- main.cs
+++ main.cs
@@ -417,7 +417,7 @@ class MainClass
 		System.IO.File.WriteAllLines(NazivFajla, ispis);
 		string[] podeljen = upis1.Split('|');
 		Console.WriteLine("Naslov: " + DeSifruj(podeljen[1],2));
-		Console.WriteLine("Posiljalac: " + DeSifruj(upis2.Substring(4), 2));
+		Console.WriteLine("Pošiljalac: " + DeSifruj(upis2.Substring(4), 2));
 		Console.WriteLine(DeSifruj(upis3, 2));
 
 		Console.WriteLine("Pritisnite Enter da biste se vratili na izbor ili Esc da se odjavite.");
